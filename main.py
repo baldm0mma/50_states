f58d98b5c876e38b8fbf1be213a7ab0cf2644092
Jev Forsberg <jevf@turbinelabs.com>
2022-04-11 10:35:51 -0600

    master/ add correctly_guessed_state_count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -8,8 +9,15 @@ screen.addshape(image_path)
 
 turtle.shape(image_path)
 
+data_path = "./50_states.csv"
+state_data = pd.read_csv(data_path)
+state_list = list(map(lambda state: state.lower(), state_data["state"].to_list()))
+print(state_list)
+
+correctly_guessed_state_count = 0
+
 current_guess = turtle.textinput(
-    title="Guess the State", prompt="Guess another State's name:"
-).lower()
+    title=f"{correctly_guessed_state_count}/50 states guessed correctly", prompt="Guess another State's name:"
+)
 
 screen.mainloop()
