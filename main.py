dce57be5992f45aa591bcf6f2ad1626bff9fc3ec
Jev Forsberg <jevf@turbinelabs.com>
2022-04-11 11:02:32 -0600

    master/ add title case

--- main.py
+++ main.py
@@ -11,13 +11,26 @@ turtle.shape(image_path)
 
 data_path = "./50_states.csv"
 state_data = pd.read_csv(data_path)
-state_list = list(map(lambda state: state.lower(), state_data["state"].to_list()))
-print(state_list)
+state_list = list(map(lambda state: state.title(), state_data["state"].to_list()))
 
 correctly_guessed_state_count = 0
 
-current_guess = turtle.textinput(
-    title=f"{correctly_guessed_state_count}/50 states guessed correctly", prompt="Guess another State's name:"
-)
+while len(state_list):
+    current_guess = turtle.textinput(
+        title=f"{correctly_guessed_state_count}/50 states guessed correctly",
+        prompt="Guess another State's name:",
+    ).title()
 
-screen.mainloop()
+    if current_guess in state_list:
+        label = turtle.Turtle()
+        label.hideturtle()
+        label.penup()
+        state = state_data[state_data["state"] == current_guess]
+        label.goto(x=float(state["x"]), y=float(state["y"]))
+        label.write(current_guess)
+        # Remove correctly guessed state from list
+        state_list.remove(current_guess)
+        # Increment `correctly_guessed_state_count`
+        correctly_guessed_state_count += 1
+
+screen.exitonclick()
